efbb27bbc0d8ca4ad3b90fd76857a45061d92fac
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-07-12 00:04:35 +0200

    Feat(app.ts):  api/auth added

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,20 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
-
+import authRouter from './modules/auth/auth.route';
 const app = express();
+
+
+
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // route de test pour vérifier que l'API fonctionne correctement
 app.get('/api/health', (req: Request, res: Response) => {
   res.status(200).json({ status: 'ok', message: 'API Job Aggregator fonctionnelle' });
 });
+
+app.use('/api/auth', authRouter);
 
 export default app;
