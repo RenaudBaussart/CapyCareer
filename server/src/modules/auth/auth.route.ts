b790cd2c9a5477bddb6373a25fc0d25dd8f435a0
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-07-12 14:14:47 +0200

    Feat(auth.route) : login route added

--- server/src/modules/auth/auth.route.ts
+++ server/src/modules/auth/auth.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { registerMember } from './auth.controller';
+import { registerMember, loginMember } from './auth.controller';
 
 const router = Router();
 
 router.post('/register', registerMember);
+router.post('/login', loginMember);
 
 export default router;
