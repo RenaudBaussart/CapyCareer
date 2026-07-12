1afc7ae3d5c76b1bfcb3f27e5edd830305dd7373
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-07-12 14:14:19 +0200

    Feat(authcontroller): http login request added

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { member } from "../members/member.schema";
 import { pool } from '../../config/database'; 
 import { AuthService } from "./auth.service"; 
+import { loginSchema } from "./auth.schema";
 
 const registerMember = async (req: Request, res: Response) => { 
     try {
@@ -32,4 +33,32 @@ const registerMember = async (req: Request, res: Response) => {
     }
 };
 
-export { registerMember };
+const loginMember = async (req: Request, res: Response) => {
+    try {
+        const loginData = loginSchema.parse(req.body);
+        
+        const authService = new AuthService(pool);
+        const token = await authService.login(loginData.username, loginData.password);
+
+        res.status(200).json({ message: "Connexion réussie.", token });
+    } catch (error: any) {
+        if (error instanceof z.ZodError) {
+            return res.status(400).json({ 
+                message: "Erreur de validation des données.", 
+                errors: error.flatten().fieldErrors 
+            });
+        }
+
+        
+        if (error.message === "USER_NOT_FOUND" || error.message === "INVALID_PASSWORD") {
+            return res.status(401).json({ message: "Identifiants incorrects." }); 
+        }
+
+        console.error("Erreur lors de la connexion du membre:", error);
+        res.status(500).json({ message: "Erreur interne du serveur." });
+    }
+};
+
+
+
+export { registerMember, loginMember };
