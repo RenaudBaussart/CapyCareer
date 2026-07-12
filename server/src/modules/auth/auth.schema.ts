344749892b0595a938821b3ee8d38c1a6fa20e48
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-07-12 14:52:09 +0200

    Fix(auth.schema.ts) : swagger and jest returned 401 error  upon empty fields instead of 400 error

--- server/src/modules/auth/auth.schema.ts
+++ server/src/modules/auth/auth.schema.ts
@@ -5,12 +5,15 @@ extendZodWithOpenApi(z);
 
 export const loginSchema = z.object({
     username: z.string()
+        .min(1, "Le pseudonyme est requis")
         .openapi({
             example: "Jojodu59",
             description: "Le pseudonyme public du membre"
         }),
-    password: z.string().openapi({
-        example: "HelloWorld0/",
-        description: "Le mot de passe en clair"
-    })
+    password: z.string()
+        .min(1, "Le mot de passe est requis")
+        .openapi({
+            example: "HelloWorld0/",
+            description: "Le mot de passe en clair"
+        })
 }).openapi("LoginRequest");
