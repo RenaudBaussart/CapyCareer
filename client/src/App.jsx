ae509c5d0a7dbf929ea43cce32997daefef31e16
Lohan <lohan.lefevre@epitech.eu>
2026-07-13 14:43:47 +0200

    Feat : added Home page, connected Navbar & App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // pages
 import Register from "./pages/Register";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* Page Register */}
         <Route path="/register" element={<Register />} />
+
+        {/* Page Accueil */}
+        <Route path="/home" element={<Home />} />
+
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+// fichier de la page d'accueil
+
+// import
+// components
+import Footer from "../components/layout/Footer";
+import Navbar from "../components/layout/Navbar";
+
+export default function Home(){
+
+    return(
+        <main>
+        <Navbar/>
+
+        
+
+        <Footer />
+        </main>
+    )
+}
