76fc81ba86eb792bc96d63eff661668a98292a9b
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-07-15 10:23:43 +0200

    Fix(member.controller): filter problem fixed,  now the current admin id on the list is filtered

--- server/src/modules/members/member.controller.ts
+++ server/src/modules/members/member.controller.ts
@@ -5,12 +5,12 @@ import { pool } from "../../config/database";
 const getAllMembersProfile = async (req: Request, res: Response) => {
     try {
         const memberService = new MemberService(pool);
-        const members = await memberService.getAllMembers();
+        const members = await memberService.getAllMembers(req.member?.id);
 
         res.status(200).json({ message: "Profils des membres récupérés avec succès.", members });
-    } catch (error: any) {
-        if (error.message === "NO_MEMBERS_FOUND") {
-           res.status(404).json({ messge: "Not found"})
+    } catch (error) {
+        if (error instanceof Error && error.message === "NO_MEMBERS_FOUND") {
+            return res.status(404).json({ message: "Aucun membre trouvé." });
         }
         console.error("Erreur lors de la récupération des profils des membres:", error);
         res.status(500).json({ message: "Erreur interne du serveur." });
