fdd1a2ab737ca8eb54f2058e1fc99101820af2fd
Baussart Renaud <renaud.baussart@proton.me>
2026-07-15 14:52:15 +0200

    feat: set id for the table post into a unique hash

--- documentation/01_data_base/data_base.sql
+++ documentation/01_data_base/data_base.sql
@@ -1,85 +1,97 @@
-CREATE TABLE `Roles` (
-  `name` VARCHAR(50) PRIMARY KEY
-);
 
-CREATE TABLE `Search_history` (
-  `PK_id` INT PRIMARY KEY AUTO_INCREMENT,
-  `search_text` VARCHAR(150) NOT NULL
-);
+Table "Roles" {
+  "name" VARCHAR(50) [pk]
+}
 
-CREATE TABLE `Job_tags` (
-  `name` VARCHAR(100) PRIMARY KEY
-);
+Table "Search_history" {
+  "PK_id" INT [pk, increment]
+  "search_text" VARCHAR(150) [not null]
+}
 
-CREATE TABLE `User_` (
-  `PK_id` INT PRIMARY KEY AUTO_INCREMENT,
-  `email` VARCHAR(100) NOT NULL,
-  `username` VARCHAR(50) NOT NULL,
-  `hashed_password` VARCHAR(255) NOT NULL,
-  `creation_date` DATE NOT NULL,
-  `last_connection` DATE NOT NULL,
-  `firstname` VARCHAR(50) NOT NULL,
-  `lastname` VARCHAR(50) NOT NULL,
-  `biography` TEXT,
-  `profil_pic_link` VARCHAR(500),
-  `FK_role_id` VARCHAR(50) NOT NULL
-);
+Table "Job_tags" {
+  "name" VARCHAR(100) [pk]
+}
 
-CREATE TABLE `Job_Offers` (
-  `PK_id` INT PRIMARY KEY AUTO_INCREMENT,
-  `name` VARCHAR(150) NOT NULL,
-  `description` TEXT NOT NULL,
-  `url` VARCHAR(500) NOT NULL,
-  `contract_type` VARCHAR(50),
-  `city` VARCHAR(150),
-  `country` VARCHAR(150),
-  `FK_user_id` INT,
-  `is_remote_job` bool,
-  `is_hybride_job` bool,
-  `publish_date` date,
-  `salary_max` int,
-  `salary_min` int,
-  `currency` char(3),
-  `content_hash` text
-);
+Table "User_" {
+  "PK_id" INT [pk, increment]
+  "email" VARCHAR(100) [not null]
+  "username" VARCHAR(50) [not null]
+  "hashed_password" VARCHAR(255) [not null]
+  "creation_date" DATE [not null]
+  "last_connection" DATE [not null]
+  "firstname" VARCHAR(50) [not null]
+  "lastname" VARCHAR(50) [not null]
+  "biography" TEXT
+  "profil_pic_link" VARCHAR(500)
+  "FK_role_id" VARCHAR(50) [not null]
 
-CREATE TABLE `Applied` (
-  `FK_user_id` INT,
-  `FK_job_offer_id` INT,
-  PRIMARY KEY (`FK_user_id`, `FK_job_offer_id`)
-);
+  Indexes {
+    email [unique]
+  }
+}
 
-CREATE TABLE `Searched` (
-  `FK_user_id` INT,
-  `FK_id_search_history` INT,
-  PRIMARY KEY (`FK_user_id`, `FK_id_search_history`)
-);
+Table "Job_Offers" {
+  "PK_content_hash" text [pk, increment]
+  "name" VARCHAR(150) [not null]
+  "description" TEXT [not null]
+  "url" VARCHAR(500) [not null]
+  "contract_type" VARCHAR(50)
+  "city" VARCHAR(150)
+  "country" VARCHAR(150)
+  "FK_user_id" INT
+  "is_remote_job" bool
+  "is_hybride_job" bool
+  "publish_date" date
+  "salary_max" int
+  "salary_min" int
+  "currency" char(3)
 
-CREATE TABLE `defined` (
-  `FK_job_offer_id` INT,
-  `FK_job_tag_name` VARCHAR(100),
-  PRIMARY KEY (`FK_job_offer_id`, `FK_job_tag_name`)
-);
+}
 
-CREATE TABLE `Banned` (
-  `PK_banned_id` INT PRIMARY KEY AUTO_INCREMENT,
-  `email` VARCHAR(100) NOT NULL
-);
+Table "Applied" {
+  "FK_user_id" INT
+  "FK_job_offer_id" INT
 
-CREATE UNIQUE INDEX `User__index_0` ON `User_` (`email`);
+  Indexes {
+    (FK_user_id, FK_job_offer_id) [pk]
+  }
+}
 
-ALTER TABLE `User_` ADD FOREIGN KEY (`FK_role_id`) REFERENCES `Roles` (`name`);
+Table "Searched" {
+  "FK_user_id" INT
+  "FK_id_search_history" INT
 
-ALTER TABLE `Job_Offers` ADD FOREIGN KEY (`FK_user_id`) REFERENCES `User_` (`PK_id`) ON DELETE SET NULL;
+  Indexes {
+    (FK_user_id, FK_id_search_history) [pk]
+  }
+}
 
-ALTER TABLE `Applied` ADD FOREIGN KEY (`FK_user_id`) REFERENCES `User_` (`PK_id`) ON DELETE CASCADE;
+Table "defined" {
+  "FK_job_offer_id" INT
+  "FK_job_tag_name" VARCHAR(100)
 
-ALTER TABLE `Applied` ADD FOREIGN KEY (`FK_job_offer_id`) REFERENCES `Job_Offers` (`PK_id`) ON DELETE CASCADE;
+  Indexes {
+    (FK_job_offer_id, FK_job_tag_name) [pk]
+  }
+}
 
-ALTER TABLE `Searched` ADD FOREIGN KEY (`FK_user_id`) REFERENCES `User_` (`PK_id`) ON DELETE CASCADE;
+Table "Banned"{
+  "PK_banned_id" INT [pk, increment]
+  "email" VARCHAR(100) [not null]
+}
 
-ALTER TABLE `Searched` ADD FOREIGN KEY (`FK_id_search_history`) REFERENCES `Search_history` (`PK_id`) ON DELETE CASCADE;
+Ref:"Roles"."name" < "User_"."FK_role_id"
 
-ALTER TABLE `defined` ADD FOREIGN KEY (`FK_job_offer_id`) REFERENCES `Job_Offers` (`PK_id`) ON DELETE CASCADE;
+Ref:"User_"."PK_id" < "Job_Offers"."FK_user_id" [delete: set null]
 
-ALTER TABLE `defined` ADD FOREIGN KEY (`FK_job_tag_name`) REFERENCES `Job_tags` (`name`) ON DELETE CASCADE;
+Ref:"User_"."PK_id" < "Applied"."FK_user_id" [delete: cascade]
+
+Ref:"Job_Offers"."PK_content_hash" < "Applied"."FK_job_offer_id" [delete: cascade]
+
+Ref:"User_"."PK_id" < "Searched"."FK_user_id" [delete: cascade]
+
+Ref:"Search_history"."PK_id" < "Searched"."FK_id_search_history" [delete: cascade]
+
+Ref:"Job_Offers"."PK_content_hash" < "defined"."FK_job_offer_id" [delete: cascade]
+
+Ref:"Job_tags"."name" < "defined"."FK_job_tag_name" [delete: cascade]
