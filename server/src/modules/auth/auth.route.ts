7cc981d7720f3c196d424dfb289c6f55aa0ca16e
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-07-16 14:23:02 +0200

    Feat: ratelimiter middleware added

--- server/src/modules/auth/auth.route.ts
+++ server/src/modules/auth/auth.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { registerMember, loginMember, logoutMember } from './auth.controller';
 import { middlewareAuth } from '../../core/middlewares/authMiddleware';
+import { loginLimiter } from '../../core/middlewares/rateLimiter';
 const router = Router();
 
 router.post('/register', registerMember);
-router.post('/login', loginMember);
+router.post('/login', loginLimiter, loginMember);
 router.post('/logout', middlewareAuth, logoutMember);
 export default router;
