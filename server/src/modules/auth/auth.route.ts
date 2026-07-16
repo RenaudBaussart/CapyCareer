119d6bbfe51d5a0585cf573c72ec2e501ad3647c
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-07-16 15:22:18 +0200

    Feat(guestMiddleware.ts): middleware to prevent a double connection on the same device

--- server/src/modules/auth/auth.route.ts
+++ server/src/modules/auth/auth.route.ts
@@ -2,9 +2,10 @@ import { Router } from 'express';
 import { registerMember, loginMember, logoutMember } from './auth.controller';
 import { middlewareAuth } from '../../core/middlewares/authMiddleware';
 import { loginLimiter } from '../../core/middlewares/rateLimiter';
+import { middlewareGuest } from '../../core/middlewares/guestMiddleware';
 const router = Router();
 
-router.post('/register', registerMember);
-router.post('/login', loginLimiter, loginMember);
+router.post('/register', middlewareGuest, registerMember);
+router.post('/login', loginLimiter, middlewareGuest, loginMember);
 router.post('/logout', middlewareAuth, logoutMember);
 export default router;
