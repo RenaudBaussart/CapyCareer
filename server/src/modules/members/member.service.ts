e5d44586c8ed066864767ed5c1d71c4f21483966
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-07-17 10:38:03 +0200

    Feat(member.service): get member by id logic added

--- server/src/modules/members/member.service.ts
+++ server/src/modules/members/member.service.ts
@@ -1,4 +1,6 @@
 import { Pool, RowDataPacket } from "mysql2/promise";
+import jwtTool from 'jsonwebtoken';
+import { ConflictError, UnauthorizedError, NotFoundError, InternalServerError } from '../../core/errors/HttpError';
 
 export class MemberService {
     private pool: Pool;
@@ -15,7 +17,7 @@ export class MemberService {
      */
     async getAllMembers(excludedMemberId?: number) {
         const connection = await this.pool.getConnection();
-        try {
+        try {            
             const [rows] = await connection.execute<RowDataPacket[]>(
                 "SELECT PK_id, email, FK_role_id, firstname, lastname, username, biography, profil_pic_link, creation_date, last_connection FROM User_", 
             );
@@ -38,7 +40,7 @@ export class MemberService {
                 : members;
 
             if (visibleMembers.length === 0) {
-                throw new Error("NO_MEMBERS_FOUND");
+                throw new NotFoundError("NO_MEMBERS_FOUND");
             }
 
             return visibleMembers;
@@ -47,4 +49,42 @@ export class MemberService {
         }
         
     }
+    /**
+     * Récupère le profil d'un membre spécifique par son ID.
+     * @param memberId L'ID du membre à récupérer.
+     * @returns Une promesse qui résout un objet représentant le profil du membre.
+     * @throws NotFoundError si aucun membre n'est trouvé avec l'ID fourni.
+     * @throws InternalServerError si une erreur de base de données se produit.
+     */
+    async getMemberById(memberId: number) {
+    const connection = await this.pool.getConnection();
+    
+    try {
+        const [rows] = await connection.execute<RowDataPacket[]>(
+            "SELECT PK_id, email, FK_role_id, firstname, lastname, username, biography, profil_pic_link, creation_date, last_connection FROM User_ WHERE PK_id = ?", 
+            [memberId] 
+        );
+
+        if (rows.length === 0) {
+            throw new NotFoundError("MEMBER_NOT_FOUND"); 
+        }
+
+        const member = rows[0] as RowDataPacket;
+
+        return {
+            id: member.PK_id,
+            email: member.email,
+            role: member.FK_role_id,
+            firstname: member.firstname,
+            lastname: member.lastname,
+            username: member.username,
+            biography: member.biography,
+            profil_pic_link: member.profil_pic_link,
+            creation_date: member.creation_date,
+            last_connection: member.last_connection
+        };
+    } finally {
+        connection.release();
+    }
+}
 }
