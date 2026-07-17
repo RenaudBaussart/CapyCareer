81407195b469383803915b5f2dbb5dbf608c0c6d
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-07-17 16:49:41 +0200

    Feat(admin.route): ban and unban route added

--- server/src/modules/admin/admin.route.ts
+++ server/src/modules/admin/admin.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { middlewareAuthAdmin } from '../../core/middlewares/adminMiddleware';
-import { getMembers } from './admin.controller';
+import { getMembers, banMember, unbanMember } from './admin.controller';
 const router = Router();
 
 router.get('/members', middlewareAuthAdmin, getMembers);
-
+router.delete('/members/ban', middlewareAuthAdmin, banMember);
+router.delete('/members/unban', middlewareAuthAdmin, unbanMember);
 export default router;
