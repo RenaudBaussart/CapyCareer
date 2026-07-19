144912a159ff85494e09f2e261d68d58139e8f65
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-07-19 21:16:18 +0200

    Feat(admin.route) : /member/:id api route added

--- server/src/modules/admin/admin.route.ts
+++ server/src/modules/admin/admin.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { middlewareAuthAdmin } from '../../core/middlewares/adminMiddleware';
-import { getMembers, banMember, unbanMember } from './admin.controller';
+import { getMembers, banMember, unbanMember, updateMembers } from './admin.controller';
 const router = Router();
 
 router.get('/members', middlewareAuthAdmin, getMembers);
 router.delete('/members/ban', middlewareAuthAdmin, banMember);
 router.delete('/members/unban', middlewareAuthAdmin, unbanMember);
+router.put('/members/:id', middlewareAuthAdmin, updateMembers);
 export default router;
