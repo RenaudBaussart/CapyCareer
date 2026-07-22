99a00606c098ca60a5f8978a5865abd1183463b0
Baussart Renaud <renaud.baussart@proton.me>
2026-07-22 14:51:06 +0200

    feat(job.offers): add route for CRUD

--- server/src/modules/job_offers/job.offers.route.js
+++ server/src/modules/job_offers/job.offers.route.js
@@ -1,8 +1,23 @@
 import { Router } from 'express';
-import { getJobOffers, getJobOfferById } from './job.offers.controller';
+import { adminAuthMiddleware } from '../auth/auth.middleware';
+import { 
+    getJobOffers, 
+    getJobOfferById, 
+    createJobOffer, 
+    updateJobOffer, 
+    deleteJobOffer, 
+    totalJobOffersCount,
+    refreshJobOffers
+} from './job.offers.controller';
+ 
+ const router = Router();
+ 
+router.get('/', getJobOffers);
+router.get('/count', totalJobOffersCount);
+router.get('/:id', getJobOfferById);
+router.post('/', adminAuthMiddleware, createJobOffer);
+router.put('/:id', adminAuthMiddleware, updateJobOffer);
+router.delete('/:id', adminAuthMiddleware, deleteJobOffer);
+router.post('/refresh', adminAuthMiddleware, refreshJobOffers);
 
-const router = Router();
-
-router.get('/job-offers', getJobOffers);
-router.get('/job-offers/:id', getJobOfferById);
 export default router;
