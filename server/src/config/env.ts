4e7dcca736d7fd831aeb8c637fdaa0a9137fa3ae
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-07-23 14:11:26 +0200

    Feat(env.ts): new variable on environment added

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -8,7 +8,18 @@ const envSchema = z.object({
   OVH_DB_PASSWORD: z.string(),
   OVH_DB_NAME: z.string(),
   OVH_DB_PORT: z.string().transform(Number),
+
+
+  ATTACK_DB_HOST: z.string(),
+  ATTACK_DB_USER: z.string(),
+  ATTACK_DB_PASSWORD: z.string(),
+  ATTACK_DB_NAME: z.string(),
+  ATTACK_DB_PORT: z.string().transform(Number),
+
+  
   SERVER_PORT: z.string().transform(Number).optional().default(5000),
+
+
 });
 
 // Parse et valider les variables d'environnement
