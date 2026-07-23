c03ffa1e274abd4e5a520b2cc341aaa02bc15aab
Lohan <lohan.lefevre@epitech.eu>
2026-07-23 11:14:26 +0200

    Feat : Connected back-end with front-end, edited some files

--- server/src/modules/job_offers/job.offers.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getJobOffers, getJobOfferById } from './job.offers.controller';
-
-const router = Router();
-
-router.get('/job-offers', getJobOffers);
-router.get('/job-offers/:id', getJobOfferById);
-export default router;
