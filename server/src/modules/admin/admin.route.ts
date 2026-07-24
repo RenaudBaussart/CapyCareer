f99b059f2a0a1990053c8c8cab994422628419f9
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-07-24 12:37:54 +0200

    chore(admin.route): static routes first

--- server/src/modules/admin/admin.route.ts
+++ server/src/modules/admin/admin.route.ts
@@ -7,12 +7,11 @@ const router = Router();
 router.get('/members', middlewareAuthAdmin, getMembers);
 router.delete('/members/ban', middlewareAuthAdmin, banMember);
 router.delete('/members/unban', middlewareAuthAdmin, unbanMember);
+
+router.get('/stats/users', middlewareAuth, middlewareAuthAdmin, getStats);
+router.get('/banned', middlewareAuthAdmin, getBannedMembers);
+
 router.patch('/members/:id/role', middlewareAuthAdmin, updateMembers);
 router.patch('/members/:id/password', middlewareAuthAdmin, updateMembers);
 router.put('/members/:id', middlewareAuthAdmin, updateMembers);
-
-// route pour les stats user
-router.get('/stats/users', middlewareAuth, middlewareAuthAdmin, getStats);
-// route pour les users banned
-router.get('/banned', middlewareAuthAdmin, getBannedMembers);
 export default router;
