e2ca1186d1524b55863d83ba64bb0ceb4336cc37
Baussart Renaud <renaud.baussart@proton.me>
2026-07-27 14:22:15 +0200

    fix(server): set good path for .env file

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,4 +1,5 @@
-import 'dotenv/config'; 
+import dotenv from 'dotenv';
+dotenv.config({ path: '../../../.env' }); 
 import { z } from 'zod';
 
 // zod schema pour valider les variables d'environnement
