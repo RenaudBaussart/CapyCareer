bff87ffcca5339b8f07e70c27b39162df41a4414
Meegy-exe <alison.dehaies@epitech.eu>
2026-07-30 13:30:07 +0200

    chore: remove commentary

--- client/src/components/auth/LoginForm.jsx
+++ client/src/components/auth/LoginForm.jsx
@@ -141,7 +141,7 @@ export default function LoginForm() {
         <div>
           <div className="flex justify-end mb-1">
             <Link to="/forgot-password" className="text-xs text-accent font-medium hover:text-accent-dark hover:underline">
-              Mot de passe oublié ?
+              {/* Mot de passe oublié ? */}
             </Link>
           </div>
 
